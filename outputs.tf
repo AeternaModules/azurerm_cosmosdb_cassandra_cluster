3e7c855a1dce002ae3d2ce495b662d9f8cc80726
Daniel Allen <dan@aeternagroup.net>
2026-01-11 01:38:55 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "cosmosdb_cassandra_clusters" {
+  description = "All cosmosdb_cassandra_cluster resources"
+  value       = azurerm_cosmosdb_cassandra_cluster.cosmosdb_cassandra_clusters
+}
+
